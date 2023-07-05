4a68201d66866a1e49110d2ab9e87d9225c144d6
TeddyG <teddygirma20@gmail.com>
2023-07-05 15:14:43 +0300

    screen.debug and logRole

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {render, screen, logRoles} from '@testing-library/react'
 import Skills from './skills'
 
 describe('Skills', () => {
@@ -33,12 +33,15 @@ describe('Skills', () => {
     })
 
     test('Start learing button is eventually displayed', async() => {
-    render(<Skills skills={skills} />)  
+    const view = render(<Skills skills={skills} />) ;
+    logRoles(view.container);
+    // screen.debug(); 
     const startLearingButton =await screen.findByRole('button', {
         name:'Start learing'
        },{
         timeout:2000,
        })
+    //    screen.debug();
        expect(startLearingButton).toBeInTheDocument();
     })
 })
